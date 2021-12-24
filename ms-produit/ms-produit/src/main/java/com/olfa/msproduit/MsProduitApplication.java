b3968cb2cb2cb35929093616e69efc0e8346bd28
olfa <olfadhaoui@gmail.com>
2021-12-24 16:27:21 +0100

    ms-produit

---
+++ ms-produit/ms-produit/src/main/java/com/olfa/msproduit/MsProduitApplication.java
@@ -0,0 +1,13 @@
+package com.olfa.msproduit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsProduitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsProduitApplication.class, args);
+	}
+
+}
---
+++ ms-produit/ms-produit/src/test/java/com/olfa/msproduit/MsProduitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olfa.msproduit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsProduitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
