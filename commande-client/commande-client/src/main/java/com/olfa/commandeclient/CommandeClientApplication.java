e4542c922ca6257d371262eb2859ea0a624b3f45
olfa <olfadhaoui@gmail.com>
2021-12-24 10:54:03 +0100

    Creating BackOff

---
+++ commande-client/commande-client/src/main/java/com/olfa/commandeclient/CommandeClientApplication.java
@@ -0,0 +1,13 @@
+package com.olfa.commandeclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommandeClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommandeClientApplication.class, args);
+	}
+
+}
---
+++ commande-client/commande-client/src/test/java/com/olfa/commandeclient/CommandeClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olfa.commandeclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommandeClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
